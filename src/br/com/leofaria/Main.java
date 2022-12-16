d1538107197c7439459c06eefc9678727733e5cc
Leonardo Oliveira Faria <leofaria.email@gmail.com>
2022-12-16 15:35:24 -0300

    COMMIT 000: Start

--- src/br/com/leofaria/Main.java
+++ src/br/com/leofaria/Main.java
@@ -1,4 +1,9 @@
 package br.com.leofaria;
 
+import br.com.leofaria.socialNetwork.EnumSocialNetwork;
+
 public class Main {
+    public static void main(String[] args) {
+        EnumSocialNetwork.INSTANCE.newNetwork();
+    }
 }
